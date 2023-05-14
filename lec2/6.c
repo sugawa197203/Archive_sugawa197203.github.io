fa7f0d83c85aeaf1b700e0189ee34f066f66e051
sugawa2018616 <96975428+sugawa197203@users.noreply.github.com>
2023-05-14 21:11:39 +0900

    lec2

---
+++ lec2/6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swap(int *a, int *b){
+	printf("a = %p, b = %p\n", a, b);
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int main(void)
+{
+	int x, y;
+	x = 5;
+	y = 3;
+	
+	printf("x = %d, y = %d\n", x, y);
+	printf("&x = %p, &y = %p\n", &x, &y);
+
+	swap(&x, &y);
+
+	printf("x = %d, y = %d\n", x, y);
+	printf("&x = %p, &y = %p\n", &x, &y);
+}
